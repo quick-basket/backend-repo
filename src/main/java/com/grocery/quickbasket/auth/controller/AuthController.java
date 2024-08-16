8f23d8a51ec536dd45812059d0e26204944f2b70
iczky <fiqra22@gmail.com>
2024-08-17 01:31:53 +0700

    feat: add skeleton for the auth controller before adding global response

---
+++ src/main/java/com/grocery/quickbasket/auth/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.grocery.quickbasket.auth.controller;
+
+import com.grocery.quickbasket.auth.service.AuthService;
+import lombok.extern.java.Log;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@Log
+public class AuthController {
+    private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(AuthService authService, AuthenticationManager authenticationManager) {
+        this.authService = authService;
+        this.authenticationManager = authenticationManager;
+    }
+
+}
