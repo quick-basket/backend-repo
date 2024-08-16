e20e351b82682ebfd94f254b3d3c2bc713b71ded
iczky <fiqra22@gmail.com>
2024-08-17 01:28:38 +0700

    feat: add RsaConfigProperties.java to encode the JWT

--- src/main/java/com/grocery/quickbasket/BackendRepoApplication.java
+++ src/main/java/com/grocery/quickbasket/BackendRepoApplication.java
@@ -1,8 +1,11 @@
 package com.grocery.quickbasket;
 
+import com.grocery.quickbasket.config.RsaConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaConfigProperties.class)
 @SpringBootApplication
 public class BackendRepoApplication {
 
