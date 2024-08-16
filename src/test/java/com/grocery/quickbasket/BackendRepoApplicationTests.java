fcb84d86b9f679de092debb9a1db10fcb7200d29
iczky <fiqra22@gmail.com>
2024-08-16 11:25:41 +0700

    init: Initialize setup Spring Boot Application

---
+++ src/test/java/com/grocery/quickbasket/BackendRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocery.quickbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
