58586c8239ce7560d926601c009b297e73dd22fe
iczky <fiqra22@gmail.com>
2024-08-16 23:33:55 +0700

    feat: add user entity for auth feature

---
+++ src/main/java/com/grocery/quickbasket/user/entity/User.java
@@ -0,0 +1,53 @@
+package com.grocery.quickbasket.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "img_profile")
+    private String imgProfile;
+
+    @ColumnDefault("false")
+    @Column(name = "is_verified")
+    private Boolean isVerified;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+/*
+ TODO [Reverse Engineering] create field to map the 'role' column
+ Available actions: Define target Java type | Uncomment as is | Remove column mapping
+    @Column(name = "role", columnDefinition = "user_role not null")
+    private Object role;
+*/
+}
