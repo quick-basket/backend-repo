a964482ef61e0ed7f274bdf8b8b5ca43eb287d9a
Fiqra Wardana <fiqra22@gmail.com>
2024-08-31 15:45:16 +0700

    feat: implement all about store management (#21)

--- src/main/java/com/grocery/quickbasket/store/service/impl/StoreServiceImpl.java
+++ src/main/java/com/grocery/quickbasket/store/service/impl/StoreServiceImpl.java
@@ -1,7 +1,11 @@
 package com.grocery.quickbasket.store.service.impl;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import com.grocery.quickbasket.exceptions.StoreNotFoundException;
+import com.grocery.quickbasket.store.dto.StoreDto;
 import org.springframework.stereotype.Service;
 
 import com.grocery.quickbasket.store.entity.Store;
@@ -18,8 +22,54 @@ public class StoreServiceImpl implements StoreService{
     }
 
     @Override
-    public List<Store> getAllStores() {
-        return storeRepository.findAll();
+    public List<StoreDto> getAllStores() {
+        List<Store> stores = storeRepository.findAll();
+        return stores.stream()
+                .map(StoreDto::fromEntity)
+                .toList();
     }
+
+    @Override
+    public StoreDto getStoreById(Long id) {
+        Optional<Store> store = storeRepository.findById(id);
+        if (store.isEmpty()) {
+            throw new StoreNotFoundException("Store not found");
+        }
+        return StoreDto.fromEntity(store.get());
+    }
+
+    @Override
+    public StoreDto addStore(StoreDto dto) {
+        Store store = StoreDto.toEntity(dto);
+        Store savedStore = storeRepository.save(store);
+
+        return StoreDto.fromEntity(savedStore);
+    }
+
+    @Override
+    public StoreDto updateStore(StoreDto dto) {
+        Optional<Store> storeOptional = storeRepository.findById(dto.getId());
+        if (storeOptional.isEmpty()) {
+            throw new StoreNotFoundException("Store not found");
+        }
+        Store currentStore = storeOptional.get();
+
+        currentStore.updateFromDto(dto);
+
+        Store savedStore = storeRepository.save(currentStore);
+
+        return StoreDto.fromEntity(savedStore);
+
+    }
+
+    @Override
+    public String deleteStore(Long id) {
+        if (!storeRepository.existsById(id)) {
+            throw new StoreNotFoundException("Store not found");
+        }
+        storeRepository.deleteById(id);
+        return "Delete successfully";
+    }
+
 
 }
