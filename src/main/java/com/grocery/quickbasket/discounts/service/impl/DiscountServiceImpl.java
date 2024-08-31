1fa34f3c75ec0c458e017d234a6601ce9217a450
DioAlfino <dioalfino95@gmail.com>
2024-08-31 19:47:40 +0700

    fix: change entitiy discount (store_id and product_id to inventory_id)

--- src/main/java/com/grocery/quickbasket/discounts/service/impl/DiscountServiceImpl.java
+++ src/main/java/com/grocery/quickbasket/discounts/service/impl/DiscountServiceImpl.java
@@ -13,27 +13,23 @@ import com.grocery.quickbasket.discounts.entity.Discount;
 import com.grocery.quickbasket.discounts.repository.DiscountRepository;
 import com.grocery.quickbasket.discounts.service.DiscountService;
 import com.grocery.quickbasket.exceptions.DataNotFoundException;
-import com.grocery.quickbasket.products.entity.Product;
-import com.grocery.quickbasket.products.repository.ProductRepository;
-import com.grocery.quickbasket.store.entity.Store;
-import com.grocery.quickbasket.store.repository.StoreRepository;
+import com.grocery.quickbasket.inventory.entity.Inventory;
+import com.grocery.quickbasket.inventory.repository.InventoryRepository;
 
 @Service
 public class DiscountServiceImpl implements DiscountService {
 
     private final DiscountRepository discountRepository;
-    private final StoreRepository storeRepository;
-    private final ProductRepository productRepository;
+    private final InventoryRepository inventoryRepository;
 
-    public DiscountServiceImpl(DiscountRepository discountRepository, StoreRepository storeRepository, ProductRepository productRepository) { 
+    public DiscountServiceImpl(DiscountRepository discountRepository, InventoryRepository inventoryRepository) { 
         this.discountRepository = discountRepository;
-        this.storeRepository = storeRepository;
-        this.productRepository = productRepository;
+        this.inventoryRepository = inventoryRepository;
     }
 
     @Override
     public List<DiscountListResponseDto> getAllDiscountsByStoreId(Long storeId) {
-        List<Discount> discounts = discountRepository.findByStoreId(storeId);
+        List<Discount> discounts = discountRepository.findAllByStoreId(storeId);
         return discounts.stream()
             .map(DiscountListResponseDto::fromEntity)
             .collect(Collectors.toList());
@@ -47,14 +43,10 @@ public class DiscountServiceImpl implements DiscountService {
 
     @Override
     public DiscountResponseDto createDiscount(DiscountRequestDto requestDto) {
-        Store store = storeRepository.findById(requestDto.getStoreId())
-            .orElseThrow(() -> new DataNotFoundException("Store not found for this id :: " + requestDto.getStoreId()));
-        Product product = productRepository.findById(requestDto.getProductId())
-            .orElseThrow(() -> new DataNotFoundException("product not found with id " + requestDto));
-        
+        Inventory inventory = inventoryRepository.findById(requestDto.getInventoryId())
+            .orElseThrow(() -> new DataNotFoundException("Inventory not found for this id :: " + requestDto.getInventoryId()));        
         Discount discount = new Discount();
-        discount.setStore(store);
-        discount.setProduct(product);
+        discount.setInventory(inventory);
         discount.setType(requestDto.getTypeAsEnum());
         discount.setValue(requestDto.getValue());
         discount.setMinPurchase(requestDto.getMinPurchase());
@@ -70,13 +62,10 @@ public class DiscountServiceImpl implements DiscountService {
     public DiscountResponseDto updateDiscount(Long id, DiscountRequestDto requestDto) {
         Discount existingDiscount = discountRepository.findById(id)
             .orElseThrow(() -> new DataNotFoundException("Store not found for this id :: " + id));
-        Store store = storeRepository.findById(requestDto.getStoreId())
-            .orElseThrow(() -> new DataNotFoundException("Store not found for this id :: " + requestDto.getStoreId()));
-        Product product = productRepository.findById(requestDto.getProductId())
-            .orElseThrow(() -> new DataNotFoundException("Store not found for this id :: " + requestDto.getProductId()));
+        Inventory inventory = inventoryRepository.findById(requestDto.getInventoryId())
+            .orElseThrow(() -> new DataNotFoundException("Inventory not found for this id :: " + requestDto.getInventoryId()));        
         
-        existingDiscount.setStore(store);
-        existingDiscount.setProduct(product);
+        existingDiscount.setInventory(inventory);
         existingDiscount.setType(requestDto.getTypeAsEnum());
         existingDiscount.setValue(requestDto.getValue());
         existingDiscount.setMinPurchase(requestDto.getMinPurchase());
@@ -86,7 +75,6 @@ public class DiscountServiceImpl implements DiscountService {
         Discount updatedDiscount = discountRepository.save(existingDiscount);
 
         return DiscountResponseDto.formDiscount(updatedDiscount);
-
     }
 
     @Override
