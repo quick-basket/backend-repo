ee77221059135b64b004b54d78af76f5bbf2d07c
iczky <fiqra22@gmail.com>
2024-09-07 01:32:58 +0700

    feat: add hibernate spatial and use postgis dialect

--- src/main/java/com/grocery/quickbasket/BackendRepoApplication.java
+++ src/main/java/com/grocery/quickbasket/BackendRepoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableCaching
 @EnableConfigurationProperties(RsaConfigProperties.class)
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class BackendRepoApplication {
 
