1c876b2241b1f659054d4ce7b0cd8281d9693dbc
DioAlfino <86925167+DioAlfino@users.noreply.github.com>
2024-10-11 21:09:08 +0700

    Feat 2/create voucher and implement to payment (#58)
    * fix: put env to local
    
    * fix: fix update payment and change payment repository
    
    * change logic update payment
    
    * fix: fix filter chain in security config
    
    * feat: create voucher and create user voucher service

--- src/main/java/com/grocery/quickbasket/vouchers/repository/VoucherRepository.java
+++ src/main/java/com/grocery/quickbasket/vouchers/repository/VoucherRepository.java
@@ -1,6 +1,8 @@
 package com.grocery.quickbasket.vouchers.repository;
 
 import java.util.Optional;
+import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +13,7 @@ public interface VoucherRepository extends JpaRepository<Voucher, Long>{
     List<Voucher> findAllByDeletedAtIsNull();
     Optional<Voucher> findByIdAndDeletedAtIsNull(Long id);
     Optional<Voucher> findByCode(String code);
+    List<Voucher> findByMinPurchaseLessThanEqualAndEndDateAfterAndDeletedAtIsNull(BigDecimal amount, Instant now);
+
+
 }
